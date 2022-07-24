8724449289cef1f420f65b7980fc453bafb3582d
C Posthuma <christiaanposthuma@gmail.com>
2022-07-24 13:12:50 +0200

    i did something :)

---
+++ stock_API.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import pandas as pd
+pd.options.display.max_rows = 9999
+pd.options.display.max_columns = 9999
+
+
+def get_stock_data_yahoo(ticker):
+    ticker_info = yf.Ticker(ticker).info
+    return ticker_info
+
+
+
+ticker = 'AAPL'
+
+
+for i in get_stock_data_yahoo(ticker):
+    print(i)
+
+
+
+
