f77b33bb3efeaa3c018c0b009d66c1e499c00258
C Posthuma <christiaanposthuma@gmail.com>
2022-07-26 07:32:30 +0200

    Added a new function:
    safe_info_to_csv ; Gets the info from yahoo and safes the info in a csv file

--- stock_API.py
+++ stock_API.py
@@ -1,5 +1,7 @@
+import csv
 import yfinance as yf
 import pandas as pd
+
 pd.options.display.max_rows = 9999
 pd.options.display.max_columns = 9999
 
@@ -9,14 +11,26 @@ def get_stock_data_yahoo(ticker):
     return ticker_info
 
 
-
-ticker = 'T'
-
-
-for i in get_stock_data_yahoo(ticker):
-    print(i)
+# for i in get_stock_data_yahoo(ticker):
+#     print(i)
 
 
+def safe_info_to_csv(ticker):
+    '''
+    This function takes a ticker and returns a csv file with the info of the ticker.
+    '''
+    try:
+        with open('Stock_info/{}.csv'.format(ticker), 'w', newline='') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=' ',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            dir = get_stock_data_yahoo(ticker)
+            for key, values in dir.items():
+                spamwriter.writerow([key, ":", values])
+        return csvfile
+    except Exception as e:
+        print(e)
+        return None
 
 
-
+ticker = 'kaasbrod'
+safe_info_to_csv(ticker)
